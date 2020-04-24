5dda6a9bf730a58d8c51262209c1596debd2d4a3
singhprayaskumar <54111107+singhprayaskumar@users.noreply.github.com>
2020-04-24 09:21:18 +0530

    v3.0

---
+++ js/guestsList.js
@@ -0,0 +1,48 @@
+let guestsList = [
+  {
+    guestNum: 0,
+    guestName: "Harish Bhat",
+    img: "images/guest-HarishBhat.jpg",
+    about: `Harish Bhat is Brand Custodian, Tata Sons. He joined the Tata
+    Group in 1987, as an officer in the Tata Administrative
+    Service. He is Chairman of Tata Coffee Ltd. He is also a
+    Director on the Boards of several other Tata Companies,
+    including Tata Global Beverages Ltd., Titan Company Ltd.,
+    Trent Ltd., Tata Starbucks Pvt.`,
+  },
+  {
+    guestNum: 1,
+    guestName: "Urooj Ashfaq",
+    img: "images/guest-UroojAshfaq.jpg",
+    about: ` One of India's youngest and most popular stand up comedians.
+    She is a talented actress and comedy writer, and began her
+    career in entertainment with an internship at AIB (All India
+    Bakchod), starring in the hit show Better Life Foundation,
+    though her big break was when she qualified as one of the
+    finalists on TLS's Queens of Comedy, a stand up comedy
+    competition`,
+  },
+  {
+    guestNum: 2,
+    guestName: "Phanindra Sama",
+    img: "images/guest-PhanindraSama.jpg",
+    about: `Phanindra Sama is the founder and former CEO of India's
+    biggest online ticket booking platform RedBus.in & Pilani Soft
+    Labs Pvt Ltd. Currently, he is the Chief Innovation Officer
+    (CIO) of Govt. of Telangana. He started Redbus back in August
+    2006 and worked there as CEO of the company for almost eight
+    years.`,
+  },
+  {
+    guestNum: 3,
+    guestName: "Ramanan Laxminarayan",
+    img: "images/guest-RamananLaxminarayan.jpg",
+    about: `Ramanan Laxminarayan is an economist and an epidemiologist. He is director of the Center for Disease Dynamics, Economics & Policy in Washington and a senior research scholar at Princeton. He is also one of India's top experts in COVID-19.`,
+  },
+  {
+    guestNum: 4,
+    guestName: "Sujit Panigrahi",
+    img: "images/guest-SujitPanigrahi.jpg",
+    about: `Sujit Panigrahi is a sports entrepreneur and the co-founder and CEO of Sequoia Fitness and Sports. He also holds many other illustrious credentials from the sports industry, including being the Additional Director General of Technology during the 2010 Delhi Commonwealth Games.`,
+  },
+];
