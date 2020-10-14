245a2a53c23f1fd7c8a62f1e757f3f9a610141d6
singhprayaskumar <54111107+singhprayaskumar@users.noreply.github.com>
2020-10-14 11:49:14 +0530

    DFTL update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This website is made by Prayas Kumar Singh for Noah Martins for his podcast 'Bits and Pieces'. Both are pursuing their Bachelors at BITS Pilani, Goa Campus.
+This website is made by Prayas Kumar Singh for Noah Martins for his podcast 'Don't Follow The Leader. Both are pursuing their Bachelors at BITS Pilani, Goa Campus.
--- js/episodesList.js
+++ js/episodesList.js
@@ -1,50 +1,74 @@
 let episodesList = [
   {
+    epNum: 10,
+    epName: "From Hyperloop to Pixxel Satellites",
+    with: "Awais Ahmed",
+    date: "26 Sep 2020",
+    about: `Awais Ahmed is the Co Founder and CEO of Pixxel, an Indian space technology start up. Pixxel is building the world's most advanced constellation of earth imaging microsatellites, and revolutionizing the Indian private space sector. They recently also raised a $5 million round of funding. Besides running Pixxel at the age of 22, Awais was also one of the founding members of India's Hyperloop team for Elon Musk's competition. If you are even mildly interested in space, entrepreneurship, engineering, or even just interesting challenges in general, this is the episode for you. Awais is genuinely one of the most inspiring people I've ever spoken to.`,
+    img:
+      "https://drive.google.com/uc?export=view&id=1mt7a_zuKRypV9kvs6NZIYECSXrDEfYwl",
+    linkYT: "https://youtu.be/JvqZtsPWUsY",
+    linkSpotify: "https://open.spotify.com/episode/3BorCNr3kRRxGzBVw9rqlf",
+  },
+  {
     epNum: 9,
+    epName: "Animation Director For Rick & Morty",
+    with: "Pete Michels",
+    date: "15 Aug 2020",
+    about: `Pete Michels is an animation director and has worked on The Simpsons, Rick & Morty and Family Guy!
+    He has also directed shows like Rugrats and Rocko's Modern Life for Nickelodeon. Throughout his career, he has worked with platforms and channels like Netflix, Fox TV, Cartoon Network, Disney, Nickelodeon, Comedy Central. He was nominated for an Emmy in 2000. Currently, he is working with Netflix for an upcoming Animated series.
+    In this episode we discuss his journey as an animator, the history of animation, careers in art, some philosophy, and his advice for young artists.`,
+    img:
+      "https://drive.google.com/uc?export=view&id=10Cl2fGoOB7K-jd2SBIz6iz-nEw9dPJqZ",
+    linkYT: "https://youtu.be/odsgAmV_wnA",
+    linkSpotify: "https://open.spotify.com/episode/4hwYvND0LOIJDpYZHMvpwL",
+  },
+  {
+    epNum: 8,
     epName: "Building One of India's Top Start Ups at 27",
     with: "Shashank Murali",
     date: "30 May 2020",
     about: `Shashank Murali is a young entrepreneur, and BITS Pilani alumnus. He sold his first start up, Edvice, while still in college. He then went on to create Tapchief(https://www.tapchief.com) , a freelancing platform, that is building a network that connects businesses to domain-specific professionals. Tapchief  is currently 3rd on the LinkedIn list of top Indian start ups. I had the opportunity to chat with this truly inspiring Indian entrepreneur, and get his take on all sorts of matters ranging from his opinion on how the world will change post the COVID-19 pandemic, to the hardest decisions he's had to make as CEO, how to get funded, and even advice that he has for young founders.`,
     img:
-      "https://drive.google.com/uc?export=view&id=1LOcgvA7oh5L39xxdvjlKCxAnyczxV3Ew",
+      "https://drive.google.com/uc?export=view&id=15D3nZyzIZXusj_WprQGHbSJ9rBQHjTJm",
     linkYT: "https://youtu.be/vDDo3MnIhdE",
     linkSpotify: "https://open.spotify.com/episode/2NMHR2ZraQUBitKFxkTR0u",
   },
   {
-    epNum: 8,
+    epNum: 7,
     epName: "Understanding Aravind SA",
     with: "Aravind SA",
     date: "9 May 2020",
     about: `Aravind SA is an internationally touring stand up comedian. We had the chance to chat with him and conversations about starting stand up, the challenges he faces and the lessons he's learned, and about life in general.`,
     img:
-      "https://drive.google.com/uc?export=view&id=1i2_IL6nZbpFuuBXrfkosji608xh7hBty",
+      "https://drive.google.com/uc?export=view&id=1J7r3_VyjlytuJ15iNNaxI2CkTyu-Dd9z",
     linkYT: "https://youtu.be/JRyco84UxVE",
     linkSpotify: "https://open.spotify.com/episode/3GFkz98uQhOmI6dUwkv3Wp",
   },
   {
-    epNum: 7,
+    epNum: 6,
     epName: "How To Get Your Start Up Funded",
     with: "Sanjay Nath",
     date: "30 Apr 2020",
     about: `We had the opportunity to interact with him when he came down to BITS Goa for an alumni gathering, where he was a panellist. This was the first episode of the series that we shot, but probably the most insightful one of them all. In this episode, we spoke to him about his journey, what he looks for in the start ups that he invests in, the WHY of starting a company, and more.`,
     img:
-      "https://drive.google.com/uc?export=view&id=1wU2QsaaByLB1eb071NsjipYKZvkzgzNo",
+      "https://drive.google.com/uc?export=view&id=1pBZHr3qM126SxNOUanvwHeaeE9smwaxI",
     linkYT: "https://youtu.be/tdKIWoxCTLM",
     linkSpotify: "https://open.spotify.com/episode/4KIQOioe49GJ7aCW0ytehD",
   },
   {
-    epNum: 6,
+    epNum: 5,
     epName: "Careers in Sports",
     with: "Sujit Panigrahi",
     date: "23 Apr 2020",
     about: `Through his work, Sujit is attempting to revolutionise the sporting industry, and change how sports are interacted with and percieved starting from a schooling level. He has many interesting ideas and insights, and is actively pursuing them as we speak. He talks about all the possible careers that one can have in sports, other than being a professional player, and how he sees the industry shifting in the short and long term.`,
     img:
-      "https://drive.google.com/uc?export=view&id=1yjQvzaodHZy5fxbDltX5nLbOAvX9dmS3",
+      "https://drive.google.com/uc?export=view&id=1C3XWir5DiFUqTUGJ7WbVepUDdOzBcWhh",
     linkYT: "https://youtu.be/n0nTt5d6XSI",
     linkSpotify: "https://open.spotify.com/episode/4fI4SSsU5VEQuB3ipPT5xo",
   },
   {
-    epNum: 5,
+    epNum: 4,
     epName: "Princeton Researcher/Social Entrepreneur",
     with: "Ramanan Laxminarayan",
     date: "13 Apr 2020",
@@ -55,19 +79,19 @@ let episodesList = [
     linkSpotify: "https://open.spotify.com/episode/08QIjDRDGLooQ3HfUKFUyy",
   },
   {
-    epNum: 4,
+    epNum: 3,
     epName: "Urooj Ashfaq",
     with: "Urooj Ashfaq",
     date: "25 Mar 2020",
     about: `Urooj Ashfaq is comedian, writer and actor, and one of the biggest stand up comics in India today. She was a regular in many AIB sketches, including the hit show Better Life Foundation. She got her big break in comedy with TLC's Queens of Comedy, a stand up competition, where she was one of the finalists. As a new comedian myself, I am a huge fan, and wa pumped that I got the opportunity to interview her. Hope you
     enjoy it.`,
     img:
-      "https://drive.google.com/uc?export=view&id=1xwV6GHjgI73_YgnjEyS4GIGPLTMwxEBT",
+      "https://drive.google.com/uc?export=view&id=1pcPzOe_67KmFbGb1MVKS0yQdpUKEc6zC",
     linkYT: "https://youtu.be/toXReuKV5qE",
     linkSpotify: "https://open.spotify.com/episode/5bqrC7qfIhKL6EcBkjxdB5",
   },
   {
-    epNum: 3,
+    epNum: 2,
     epName: "Tata Sons Brand Custodian",
     with: "Harish Bhat",
     date: "21 Feb 2020",
@@ -76,17 +100,6 @@ let episodesList = [
       "https://drive.google.com/uc?export=view&id=1Sp-Bnko0MWtz86Ue5zuoLTh4R1apj5ki",
     linkYT: "https://youtu.be/hj3fCQjT0Fg",
     linkSpotify: "",
-  },
-  {
-    epNum: 2,
-    epName: "How We Prepared for BITSAT",
-    with: "friends",
-    date: "8 Feb 2020",
-    about: `Noah, Rayan, Rohan and Gada discuss how they prepared for BITSAT, whether or not one should attempt the extra 12 bonus questions, BITS Goa life and culture, and more. A must watch for all BITS aspirants who are preparing for BITSAT 2020.`,
-    img:
-      "https://drive.google.com/uc?export=view&id=1jnIrOVW3crEX3XGELVkigsWIAibTUJnv",
-    linkYT: "https://youtu.be/VrFQT2Gbzu4",
-    linkSpotify: "https://open.spotify.com/episode/5Vi2B8Nmw9vGUOQIHNmYl3",
   },
   {
     epNum: 1,
